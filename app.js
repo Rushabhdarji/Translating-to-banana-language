627466f53d7b794379710e88d8a893b32985112c
Rushabhdarji <52121432+Rushabhdarji@users.noreply.github.com>
2021-01-03 10:14:40 +0530

    Added button, and attached event-listener

---
+++ app.js
@@ -0,0 +1,13 @@
+var btnTranslate = document.querySelector('#btn-translate');
+
+
+
+//connecting button with event-handler;
+btnTranslate.addEventListener("click", eventHandler());
+
+
+
+//function eventHandler
+function eventHandler() {
+    console.log("clicked!");
+}
