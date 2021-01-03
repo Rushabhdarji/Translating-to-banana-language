972ca4fb1211cf1e1fd9e22b04b6c37d06b22e2a
Rushabhdarji <52121432+Rushabhdarji@users.noreply.github.com>
2021-01-03 13:17:06 +0530

    wired the textarea as input

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 var btnTranslate = document.querySelector('#btn-translate');
-
-
-
-//connecting button with event-handler;
-btnTranslate.addEventListener("click", eventHandler());
-
-
+var txtInput = document.querySelector('#input-text');
 
 //function eventHandler
 function eventHandler() {
     console.log("clicked!");
-}
+    console.log("input",txtInput.value);
+};
+//connecting button with event-handler;
+btnTranslate.addEventListener("click", eventHandler);
