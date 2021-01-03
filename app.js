598d793aaa567c2864a8c421de3a0c87eb83626f
Rushabhdarji <52121432+Rushabhdarji@users.noreply.github.com>
2021-01-03 17:58:51 +0530

    comments of what mistakes i created

--- app.js
+++ app.js
@@ -7,4 +7,6 @@ function eventHandler() {
     console.log("input",txtInput.value);
 };
 //connecting button with event-handler;
+//If don't specify the whole function after the event in addEventListener then we just need to specify the nsame of the function.
+//note:- we don't need to call the function we just need to specify the name of that function.
 btnTranslate.addEventListener("click", eventHandler);
